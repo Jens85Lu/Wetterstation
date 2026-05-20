150cb1d9bdec9c46e620bdf367be04ef8206ebf6
Jens Ludwig <jens.lu@web.de>
2026-05-20 16:57:27 +0200

    Add a little animation (screensaver)

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ void setup() {
   Serial.begin(115200);
   dht_init();
   display_init();
-
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
--- src/weather_ui.cpp
+++ src/weather_ui.cpp
@@ -6,21 +6,44 @@
 #include <U8g2lib.h>
 
 extern U8G2_SH1106_128X64_NONAME_F_HW_I2C display;
+static bool lastEdge = false;
 
 void weather_show(float temp, float humidity) {
+  static int x = 0; // movement in x direction
+  static int y = 0; // movement in y direction
   char tempStr[16];
   char humidityStr[16];
   display.clearBuffer();
   display.setFont(u8g2_font_crox4hb_tf);
   dtostrf(temp, 4, 1, tempStr);
   dtostrf(humidity, 4, 1, humidityStr);
-  display.drawStr(0, 20, "T: ");
-  display.drawStr(70,20, tempStr);
-  display.drawStr(110,20, "\xB0""C"); // Gradzeichen
-  display.drawStr(0, 40, "H: ");
-  display.drawStr(70, 40, humidityStr);
-  display.drawStr(110, 40, "%");
+  display.drawStr(x, 20 + y, "T: ");
+  display.drawStr(70,20 + y, tempStr);
+  display.drawStr(110,20 + y, "\xB0""C"); // Gradzeichen
+  display.drawStr(x, 40 + 1.1*y, "H: ");
+  display.drawStr(70, 40 + 1.1*y, humidityStr);
+  display.drawStr(110, 40 + 1.1*y, "%");
 
   display.sendBuffer();
-  
+  // Einfache Animation: Text bewegt sich von rechts nach links und zurück
+  if (x > 20) {
+    lastEdge = 1;
+  } else if (x < 0) {
+    lastEdge = 0;
+  }
+  if (lastEdge == 1) {
+    x--;
+  } else {
+    x++;
+  }
+  if (y > 20) {
+    lastEdge = 1;
+  } else if (y < 0) {
+    lastEdge = 0;
+  }
+  if (lastEdge == 1) {
+    y--;
+  } else {
+    y++;
+  }
 }
